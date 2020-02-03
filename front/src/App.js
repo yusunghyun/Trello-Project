6c7fdac40c0eff6e32d5fd957c7623745e2534c2
GwangJin Byeon <qusrhkdwls45.gmail.com>
2020-02-04 05:09:35 +0900

    board 컨테이너 완성

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
+import BoardContainer from './components/board/BoardContainer';
 
 function App() {
   return (
     <div>
-      
+      <Link to="/boardContainer"></Link>
+
       <Switch>
-        
+        <Route path="/:boardContainer" component={BoardContainer}></Route>
       </Switch>
     </div>
   );
--- front/src/components/board/BoardContainer.js
+++ front/src/components/board/BoardContainer.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  container: {
+    backgroundColor: '#cfe8fc',
+    height: '100vh'
+  }
+})
 
 const BoardContainer = () => {
+  const classes = useStyles();
   return (
-    <Container>
-      
-    </Container>
+    <React.Fragment>
+    {/* React.Fragment, CssBaseline이 있어야 공백 없이 Container가 채워지는데 왜 그러는지는 모르겠음 */}
+      <CssBaseline />
+      <Container className={classes.container}>
+        
+      </Container>
+    </React.Fragment>
   );
 };
 
