face6ba4a6d44222b20a7a44aa3770bb65ac401e
GwangJin Byeon <qusrhkdwls45.gmail.com>
2020-02-04 03:18:13 +0900

    학원에서 배운 기본 세팅

---
+++ front/src/components/board/BoardContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+const BoardContainer = () => {
+  return (
+    <Container>
+      
+    </Container>
+  );
+};
+
+export default BoardContainer;
